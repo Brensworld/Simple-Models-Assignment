dd06cbe039ec139ea116c40fcc6f5199abafb41d
Brensworld <brensworld83@gmail.com>
2025-11-01 15:11:05 -0400

    added dog model and page 3 view

---
+++ server/models/Dog.js
@@ -0,0 +1,36 @@
+const mongoose = require('mongoose');
+
+let DogModel={};
+
+const DogSchema=new mongoose.Schema({
+    name:{
+        type:String,
+        required:true,
+        trim:true,
+        unique:false,
+    },
+
+    breed:{
+        type:String,
+        required:true,
+        trim:true,
+        unique:false,
+    },
+
+    age:{
+        type:Number,
+        required:true,
+        min:0,
+        unique:false,
+    },
+
+    createdDate:{
+        type:Date,
+        default:Date.now
+    }
+
+});
+
+DogModel=mongoose.model('Dog',DogSchema);
+
+module.exports=DogModel;
